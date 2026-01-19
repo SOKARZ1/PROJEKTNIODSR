62436849185902978c61515bf7bdd1bb1fa01f66
Ku8zone <jakub.dudziak6@gmail.com>
2026-01-19 14:44:01 +0100

    Add launch files to data_files in setup.py
    Added a data_files entry for launch files.

--- src/my_robot_project/setup.py
+++ src/my_robot_project/setup.py
@@ -12,7 +12,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # DODAJ TĘ LINIĘ:
         (os.path.join('share', package_name, 'launch'),     
         glob('launch/*.py')),
     ],
