099d3bf59268d7ef096453764b0d6b5da671427f
SOKARZ1 <kacper.gwizdon@wp.pl>
2026-01-19 15:58:56 +0100

    Update setup.py

--- src/my_robot_project/setup.py
+++ src/my_robot_project/setup.py
@@ -17,8 +17,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='huj',
-    maintainer_email='huj@todo.todo',
+    maintainer='git',
+    maintainer_email='git@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
