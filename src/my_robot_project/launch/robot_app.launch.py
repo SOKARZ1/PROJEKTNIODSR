0a8fc66bb2ad537390654c78cc09c1e7a64ecc21
SOKARZ1 <kacper.gwizdon@wp.pl>
2026-01-19 14:38:06 +0100

    Update robot_app.launch.py

--- src/my_robot_project/launch/robot_app.launch.py
+++ src/my_robot_project/launch/robot_app.launch.py
@@ -5,14 +5,13 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # Znajdujemy pakiet z opisem UR (ten który sklonowałeś)
+   
     ur_description_pkg = FindPackageShare("ur_description")
     
-    # Ścieżka do pliku Xacro UR5
+    
     urdf_file = PathJoinSubstitution([ur_description_pkg, "urdf", "ur.urdf.xacro"])
 
-    # Generowanie opisu robota komendą xacro
-    # Parametry: name=ur5, ur_type=ur5
+   
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
@@ -28,7 +27,7 @@ def generate_launch_description():
     robot_description = {"robot_description": robot_description_content}
 
     return LaunchDescription([
-        # 1. Robot State Publisher (z prawdziwym modelem UR5)
+        
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
@@ -36,7 +35,7 @@ def generate_launch_description():
             parameters=[robot_description],
         ),
         
-        # 2. RViz2
+        #  RViz2
         Node(
             package="rviz2",
             executable="rviz2",
@@ -44,14 +43,14 @@ def generate_launch_description():
             output="screen",
         ),
 
-        # 3. Twój symulator kamery
+        #  symulator kamery
         Node(
             package='my_robot_project',
             executable='camera_sim',
             name='camera_simulator',
         ),
 
-        # 4. Twój kontroler UR5
+        # kontroler UR5
         Node(
             package='my_robot_project',
             executable='controller',
