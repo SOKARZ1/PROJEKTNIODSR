096b22221d012b16adad03eff5a6e6c07276c27f
SOKARZ1 <kacper.gwizdon@wp.pl>
2026-01-19 14:39:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-# Załaduj środowisko ROS 2 Jazzy
+
 source /opt/ros/jazzy/setup.bash
 
-# Załaduj Twój zbudowany workspace
+
 if [ -f /root/ros2_ws/install/setup.bash ]; then
   source /root/ros2_ws/install/setup.bash
 fi
 
-# Wykonaj komendę przekazaną do dockera
 exec "$@"
